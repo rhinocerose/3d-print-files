dbe5c69b5b39a633609c730518748a5a6700a54d
rhinocerose <you@example.com>
2026-02-19 15:08:11 -0500

    Added missing

---
+++ storage/systems/gridfinity/labels/print_dict.py
@@ -0,0 +1,32 @@
+print_list = {
+    "Diodes" : {
+        "symbol": "Diode-COM-Standard",
+        "line1":  "Through Hole",
+        "line2":  "Diodes",
+        "file":   "th-diode-label"
+    },      
+    "Panel-Pot" : {
+        "symbol": "Resistor-IEEE-Potentiometer",
+        "line1":  "Panel",
+        "line2":  "Potentiometer",
+        "file":   "panel-pot-label"
+    },      
+    "Panel-Pot" : {
+        "symbol": "Resistor-IEEE-Potentiometer",
+        "line1":  "PCB",
+        "line2":  "Potentiometer",
+        "file":   "panel-pcb-label"
+    },    
+    "Trimpot" : {
+        "symbol": "Resistor-IEEE-Trimmer",
+        "line1":  "Trimpot",
+        "line2":  None,
+        "file":   "trimpot-label"
+    },   
+    "Switch" : {
+        "symbol": "Switch-COM-SPST",
+        "line1":  "Switches",
+        "line2":  None,
+        "file":   "switches-label"
+    },        
+}
